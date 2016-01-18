41028344ad0d50cbdfa51ade0af9e618a6b3459f
nicolaprezza <nicolapr@gmail.com>
2016-01-18 17:19:52 +0100

    added 7z compression

--- wiki_get.sh
+++ wiki_get.sh
@@ -1,7 +1,11 @@
 # Download all Wikipedia version of input page starting from 
-# January 1st of input year.
+# January 1st of the specified input year. The downloaded dataset
+# Is automatically compressed with 7-Zip
 #
 # usage: wiki_get.sh <page> <start_year> <output_file_name>
+#
+# File "output_file_name.7z" is created. The file contains several data dumps, 
+# each containing 1K page versions (except possibly the last).
 #
 # Note: to download ALL versions of the page, use start_year=0
 #
@@ -13,12 +17,14 @@ page=$1
 year_start=$2
 outfile=$3
 
-rm $outfile
+rm $outfile.7z
 
 echo "Downloading all Wikipedia version of page "$page
 echo "output file: "$outfile
 
 start_timestamp=$year_start-01-01T00:00:00Z
+
+tot_rev=0
 
 while [ "$start_timestamp" != "" ]
 do
@@ -42,8 +48,21 @@ do
 	then
 		echo "No more records. Search terminated."
 	else
-		#append to archive and remove temporary dump
-		zcat current_dump.gz >> $outfile
+		#append to archive
+
+		echo "adding file "dump_$start_timestamp.xml" to archive"
+
+		zcat current_dump.gz > dump_$start_timestamp.xml
+		7z u $outfile.7z dump_$start_timestamp.xml
+	
+		#count how many revisions have been added
+		rev=`cat dump_$start_timestamp.xml | grep \<timestamp\> | wc -l`
+		tot_rev=$((tot_rev+rev))
+	
+		echo "added "$rev" revisions"
+
+		rm dump_$start_timestamp.xml
+
 	fi	
 
 	rm current_dump.gz
@@ -51,6 +70,6 @@ do
 done
 
 echo ""
-echo "Number of downloaded revisions: "`cat $outfile | grep \<timestamp\> | wc -l`
+echo "Number of downloaded revisions: "$tot_rev
 
 
